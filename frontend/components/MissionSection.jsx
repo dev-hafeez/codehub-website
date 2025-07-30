41bd57dcf5021447f035b75d3b6cae6d559d7c99
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-07-30 21:46:18 +0500

    added contents of mission section

--- frontend/components/MissionSection.jsx
+++ frontend/components/MissionSection.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './MissionsSection.css';
+
 
 const MissionSection = () => {
   return (
-    <div>MissionSection</div>
+    <div>
+      <div className="container-fluid border justify-content-center p-5">
+
+      <div className="border container col-lg-5 justify-content-center align-items-center">
+        <h1 className="text-center heading fs-1">Our Mission</h1>
+      <p className="lead text-center mission-text">
+        Our mission is to empower students through technology, innovation, and collaboration. We aim to create a dynamic learning environment where students can enhance their skills, share ideas, and grow as future leaders in tech. Through workshops, competitions, and community events, we strive to inspire and uplift the next generation of computing professionals.
+      </p>
+      <div className="text-center">  
+        <button type="button" className="btn learn-more-btn">Learn More </button>
+      </div>
+      
+      </div>
+      
+    </div>
+    </div>
+      
   )
 }
+
 
 export default MissionSection
