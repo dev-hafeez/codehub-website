75e09c3d50da28eacd1d7170ec1e554ebf32ea1b
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-07-31 00:50:11 +0500

    Create LandingPage.jsx

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MissionSection from '../components/MissionSection'
+import ClubsSection from '../components/ClubsSection'
+
+const LandingPage = () => {
+  return (
+    // <div>LandingPage</div>
+    
+      // <MissionSection />
+      <ClubsSection />
+
+  )
+}
+
+export default LandingPage
