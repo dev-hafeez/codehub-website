ebe37bd2c8c335de88e2ea36449ce21e9df32c50
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-08-02 20:37:16 +0500

    Create BlogCard.jsx

---
+++ frontend/src/components/BlogCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/BlogCard.css';
+
+const BlogCard = () => {
+  return (
+    <div>BlogCard</div>
+  )
+}
+
+export default BlogCard
