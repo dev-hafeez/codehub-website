1eaec838ccd46fafd39e7ba8d07e6fd5848f20d6
astrophileAqsa123 <aqsaarif5678@gmail.com>
2025-08-02 12:32:17 +0500

    Fixing pages

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,31 +1,11 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-<<<<<<< HEAD
-import "./Hero.css";
-=======
 import "../styles/Hero.css";
->>>>>>> feature/achievements-blog
 import RightCol from "./rightCol";
 import LeftCol from "./LeftCol";
 
 const Hero = () => {
   return (
-<<<<<<< HEAD
-    <section className="hero-section text-white py-5">
-      <Container>
-        <Row className="align-items-center">
-
-          {/* Left Section */}
-          <Col md={6} className="text-center mb-4 mb-md-0">
-            <LeftCol />
-          </Col>
-
-          {/* Right Section */}
-          <Col md={6}>
-            <RightCol />
-          </Col>
-        </Row>
-=======
     <section className="hero-section text-white py-5 ">
       <Container>
         <Row className="align-items-center">
@@ -37,7 +17,6 @@ const Hero = () => {
   </Col>
 </Row>
 
->>>>>>> feature/achievements-blog
       </Container>
     </section>
   );
