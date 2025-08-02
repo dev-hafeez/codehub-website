e21039280dce8c1327579acb0984f4fa8a75a100
a_Aqsa123 <aqsaarif5678@gmail.com>
2025-08-02 12:36:27 +0500

    Update LeftCol.jsx

--- frontend/src/components/LeftCol.jsx
+++ frontend/src/components/LeftCol.jsx
@@ -1,43 +1,12 @@
-<<<<<<< HEAD
-import React from 'react'
-import "./Hero.css";
-=======
+
 import React from 'react';
 import "../styles/Hero.css";
->>>>>>> feature/achievements-blog
+
 
 const LeftCol = () => {
   return (
     <>
-<<<<<<< HEAD
-      <img
-        src="/acm-comsats-wah-chapter.png"
-        alt="ACM Logo"
-        className="img-fluid mb-3 hero-logo"
-        style={{ maxHeight: "120px" }}
-      />
-      <h1 className="display-5 fw-bold">ACM CHAPTER WAH</h1>
 
-      <div className="text-center d-flex justify-content-center  gap-4 mt-3">
-        <div>
-          <h3 className="mb-0 fw-bold">15+</h3>
-          <p className="mb-0">Courses</p>
-        </div>
-        <div>
-          <h3 className="mb-0 fw-bold">200+</h3>
-          <p className="mb-0">Members</p>
-        </div>
-        <div>
-          <h3 className="mb-0 fw-bold">10+</h3>
-          <p className="mb-0">Events</p>
-        </div>
-      </div>
-    </>
-  )
-}
-
-export default LeftCol
-=======
       <div className="text-center">
         <img
           src="/acm-comsats-wah-chapter.png"
@@ -67,4 +36,4 @@ export default LeftCol
 
 export default LeftCol;
 
->>>>>>> feature/achievements-blog
+
