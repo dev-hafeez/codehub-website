3c346c48d7cb5c9ab606cba8805d7d7cc3d70f96
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-08-03 00:23:36 +0500

    Update BlogCard.jsx

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/BlogCard.css";
-import carImg from "../assets/car-image.jpg";
-import authorImg from "../assets/author-1.jpg";
 
-const BlogCard = () => {
+const BlogCard = ({title, author,date,tag, image, authorImg} ) => {
   return (
     <div className="blogCard-wrapper">
       <div className="blogCard-container border container col-6 col-md-4 col-lg-3 col-xl-2">
         <div className="blog-img-container py-3">
-          <img src={carImg} alt="CarImage" className="blog-image img-fluid" />
+          <img src={image} alt="CarImage" className="blog-image img-fluid" />
         </div>
         <div className="blog-tag">
-          <span className="tag-text">Technology</span>
+          <span className="tag-text">{tag}</span>
         </div>
         <div className="blog-title pt-2">
           <h2 className="blog-title-text">
-            The Impact of Technology on the WorkPlace: How Technology is
-            Changing
+            {title}
           </h2>
         </div>
         <div className="d-flex pt-3 flex-row justify-content-start bottom">
@@ -28,8 +25,8 @@ const BlogCard = () => {
               className="author-image img-fluid"
             />
           </span>
-          <span className="d-flex pt-2 ps-3 blog-author-name">John Doe</span>
-          <span className="d-flex pt-2 ps-3 blog-date">March 15, 2023</span>
+          <span className="d-flex pt-2 ps-3 blog-author-name">{author}</span>
+          <span className="d-flex pt-2 ps-3 blog-date">{date}</span>
         </div>
       </div>
     </div>
