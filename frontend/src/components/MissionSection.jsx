02e559ea75dc8806b2edc2098d8b9f1386833b8e
a_Aqsa123 <aqsaarif5678@gmail.com>
2025-08-02 12:37:00 +0500

    Update MissionSection.jsx

--- frontend/src/components/MissionSection.jsx
+++ frontend/src/components/MissionSection.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/MissionsSection.css';
-<<<<<<< HEAD
-import Brush from '../../assets/brush.png';
-import Waves from '../../assets/waves.png';
-=======
+
 import Brush from '../assets/brush.png';
 import Waves from '../assets/waves.png';
->>>>>>> feature/achievements-blog
+
 
 const MissionSection = () => {
   return (
