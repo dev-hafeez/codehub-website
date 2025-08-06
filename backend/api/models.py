c94a3d44f0ee996ba2addaf9b1670ccdb181e733
NextStep-IM <malikharis2003@gmail.com>
2025-08-07 01:19:36 +0500

    Add default value for roll number field

--- backend/api/models.py
+++ backend/api/models.py
@@ -25,7 +25,7 @@ class Student(models.Model):
     Model representing a student. Inherits from User using OneToOne relationship.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student')
-    roll_no = models.CharField(max_length=20)
+    roll_no = models.CharField(max_length=20, default="")
     club = models.CharField(max_length=100)
 
 
