c8ed87546233560eb3f9174ab1a133caa0114cca
Umar-92 <umarazam2222@gmail.com>
2025-08-07 03:29:21 +0500

    login page (#29)
    * Backend/user auth (#9)
    
    * Create requirements.txt
    
    * Added Student and Achievements Mode
    
    * Update database port
    
    * Update settings.py
    
    ---------
    
    Co-authored-by: NextStep-IM <malikharis2003@gmail.com>
    
    * Added responsive login page with gradient arc design
    
    ---------
    
    Co-authored-by: Adeel Asghar <adeelyt157@gmail.com>
    Co-authored-by: NextStep-IM <malikharis2003@gmail.com>
    Co-authored-by: dev-hafeez <159642563+dev-hafeez@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,30 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.jsx';
-import './App.css'
-import { Button, Container, Alert } from 'react-bootstrap';
+import LoginPage from './pages/LoginPage.jsx';
 import BlogListingPage from './pages/BlogListingPage.jsx';
-import SinglePostPage from './pages/SinglePostPage.jsx'
-import { sampleBlog } from "./SampleData.js";
+import SinglePostPage from './pages/SinglePostPage.jsx';
 import ArticlePage from './pages/ArticlePage.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
+import './App.css';
+import { Button, Container, Alert } from 'react-bootstrap';
+import { sampleBlog } from "./SampleData.js";
 
-
-function App() { 
+function App() {
   return (
     <>
-<Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-         <Route path="/blogs" element={<BlogListingPage/>} />
-         <Route path="/blog/:id" element={<SinglePostPage/>}/>
-         <Route path="/article" element={<ArticlePage/>}/>
-         <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-    </Router>
-
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/blogs" element={<BlogListingPage />} />
+          <Route path="/blog/:id" element={<SinglePostPage />} />
+          <Route path="/article" element={<ArticlePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
