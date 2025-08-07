88cde61bd668b6f5e46626b9ed25d0367708c926
NextStep-IM <malikharis2003@gmail.com>
2025-08-07 21:02:07 +0500

    Correct typo

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -6,4 +6,4 @@ class IsLead(permissions.BasePermission):
     message = 'Only user type: \'lead\' is allowed to use this endpoint.'
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'lead'
+        return request.user.is_authenticated and request.user.role == 'LEAD'
