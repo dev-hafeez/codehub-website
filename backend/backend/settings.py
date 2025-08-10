865d002e84379f1dfbc3a3cf04d2fc903dfe55c8
Malik Haris Ali <malikharis2003@gmail.com>
2025-08-10 20:42:59 +0500

    Revert "feat: Add blogs management feature (#33)" (#34)
    This reverts commit 467c1682b55f078b2015421a3bf59893c3cf8f33.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'USER': 'backend',
         'PASSWORD': 'backend-test-pass',
         'HOST': '0.tcp.ap.ngrok.io',
-        'PORT': '12456', # This will change if the host machine shuts down.
+        'PORT': '14281', # This will change if the host machine shuts down.
                          # Currently, there is no easy way to automatically change this
                          # without spending money.
     }
