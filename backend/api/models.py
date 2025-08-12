dedad5a8e5b16be7e57ffd3303a6badefbb3856a
yoursmaddyy <adeelyt157@gmail.com>
2025-08-12 20:09:12 +0500

    updated blogs model with image retrieval

--- backend/api/models.py
+++ backend/api/models.py
@@ -32,30 +32,30 @@ class Student(models.Model):
     club = models.CharField(max_length=100)
 
 
-# def blog_image_upload_path(instance, filename):
-#   """
-#    Store blog images inside a folder for each blog ID.
-#    Example: blog_images/<blog_id>/<filename>
-#    """
-#    return os.path.join('blog_images', str(instance.id), filename) 
+def blog_image_upload_path(instance, filename):
+    """
+    Store blog images inside a folder for each blog ID.
+    Example: blog_images/<blog_id>/<filename>
+    """
+    return os.path.join('blog_images', str(instance.id), filename) 
     
-# class Blog(models.Model):
-#     """
-#     Model representing a blog post created by users (typically students or leads).
-#     """
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     title = models.CharField(max_length=200)
-#     content = models.TextField()
-#     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
-#     def displayPreview(self):
-#         """
-#         Returns a preview (first 100 characters) of the blog content.
-#         """
-#         return self.content[:100]
-#
+class Blog(models.Model):
+    """
+    Model representing a blog post created by users (typically students or leads).
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def displayPreview(self):
+        """
+        Returns a preview (first 100 characters) of the blog content.
+         """
+        return self.content[:100]
+
 # class Event(models.Model):
 #     """
 #     Model representing an event created by an admin.
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -33,3 +33,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
