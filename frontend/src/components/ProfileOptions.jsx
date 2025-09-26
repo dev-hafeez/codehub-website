eeb69e1c196e61b4785df843be025251f53646bc
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-09-27 00:13:15 +0500

    Create ProfileOptions.jsx

---
+++ frontend/src/components/ProfileOptions.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./../styles/ProfileOptions.css";
+import useAuthStore from "../store/authStore";
+
+
+const ProfileOptions = () => {
+  const navigate = useNavigate();
+  const { logout } = useAuthStore();
+  const handleLogout = () => {
+    logout();
+    window.location.href = "/login";
+  };
+  
+  return (
+    <div className="profile-options-box">
+      <button
+        className="option-btn"
+        onClick={() => navigate("/edit-profile")}
+      >
+        Edit Profile
+      </button>
+
+      <button
+        className="option-btn"
+        onClick={() => navigate("/reset-password")}
+      >
+        Reset Password
+      </button>
+
+      <button
+        className="option-btn"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
+      
+    </div>
+  );
+};
+
+export default ProfileOptions;
